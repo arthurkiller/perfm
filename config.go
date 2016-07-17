5d4d9b73dff494fd3692169043d8e235a1762d90
arthur <arthur-lee@qq.com>
2016-07-17 23:47:13 +0800

    add the support of the historgam

--- config.go
+++ config.go
@@ -3,4 +3,5 @@ package perfM
 type Config struct {
 	Frequency  int //set for the sampling frequency
 	BufferSize int //set for the global time channel buffer size
+	BinsNumber int //set the histogram bins number
 }
--- perfM.go
+++ perfM.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"time"
 
-	_ "github.com/VividCortex/gohistogram"
+	hist "github.com/VividCortex/gohistogram"
 )
 
 type Job interface {
@@ -28,13 +28,14 @@ type PerfMonitor interface {
 }
 
 type perfMonitor struct {
-	done           chan bool          //stor the perfM
-	counter        int                //count the sum of the request
-	startTime      time.Time          //keep the start time
-	timer          time.Timer         //the frequency sampling timer
-	GlobalChannel  chan time.Duration //get the request cost from every done()
-	localCount     int                //count for the number in the sampling times
-	localTimeCount time.Duration      //count for the sampling time total costs
+	done           chan bool              //stor the perfM
+	counter        int                    //count the sum of the request
+	startTime      time.Time              //keep the start time
+	timer          time.Timer             //the frequency sampling timer
+	GlobalChannel  chan time.Duration     //get the request cost from every done()
+	localCount     int                    //count for the number in the sampling times
+	localTimeCount time.Duration          //count for the sampling time total costs
+	histogram      *hist.NumericHistogram //used to print the histogram
 }
 
 func New(conf Config) {
@@ -43,6 +44,7 @@ func New(conf Config) {
 		startTime:     time.Time,
 		timer:         time.NewTimer(time.Second * int64(conf.Frequency)),
 		GlobalChannel: make(chan time.Duration, conf.BufferSize),
+		histogram:     hist.NewHistogram(conf.BinsNumber),
 	}
 }
 
@@ -54,9 +56,9 @@ func (p *perfMonitor) Start() error {
 			p.counter++
 			p.localCount++
 			p.localTimeCount += cost
+			p.histogram.Add(float64(cost))
 		case <-p.timer:
-			//TODO:show the courently performence info
-			log.Println("Qps: ", p.localCount, "")
+			log.Println("Qps: ", p.localCount, "Avg Latency: ", p.localTimeCount/int64(p.localCount))
 			p.localCount = 0
 			p.localTimeCount = 0
 		case <-p.done:
@@ -69,6 +71,10 @@ func (p *perfMonitor) Start() error {
 func (p *perfMonitor) Stop() error {
 	//TODO:show the info of the performence test
 	p.done <- true
+	//show the summery
+
+	// here show the histogram
+	log.Println(p.histogram.String())
 	return nil
 }
 
