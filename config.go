32ce10b009a0addda546c05bae99db16acb016b7
lza@arthur <lza@meitu.com>
2016-10-10 14:26:42 +0800

    add comment

--- config.go
+++ config.go
@@ -1,5 +1,6 @@
 package perfM
 
+//Config define the Config about perfM
 type Config struct {
 	Frequency  int //set for the sampling frequency
 	BufferSize int //set for the global time channel buffer size
