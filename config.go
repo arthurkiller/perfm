6ed2f83156cc76cabafd7d00cda2297c16faf9e6
lza@arthur <lza@meitu.com>
2016-10-19 00:11:11 +0800

    change into the with style for config

--- config.go
+++ config.go
@@ -6,3 +6,36 @@ type Config struct {
 	BufferSize int //set for the global time channel buffer size
 	BinsNumber int //set the histogram bins number
 }
+
+//new gen the config
+func NewConfig(options ...Options) Config {
+	c := Config{}
+	for _, o := range options {
+		o(&c)
+	}
+	return c
+}
+
+//Options define the options of congif
+type Options func(*Config)
+
+//WithFrequency set the frequency
+func WithFrequency(i int) Options {
+	return func(o *Config) {
+		o.Frequency = i
+	}
+}
+
+//WithBuffersize set the buffer size of config
+func WithBufferSize(i int) Options {
+	return func(o *Config) {
+		o.BufferSize = i
+	}
+}
+
+//Withbinsnumber set the bins number of config
+func WithBinsNumber(i int) Options {
+	return func(o *Config) {
+		o.BinsNumber = i
+	}
+}
